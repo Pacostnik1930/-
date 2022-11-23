7cd92f8c8089fe754786cc023a8bfd3e7f58690a
Гойхман Петр <pgoyhman@mail.ru>
2022-11-23 10:28:22 +0300

    Hometask02

---
+++ hometask02/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+if(a > b && a > c)
+{
+Console.WriteLine(a);
+}
+else
+
+if(b > c)
+{
+Console.WriteLine(b);
+}
+else
+{
+Console.WriteLine(c);
+}
